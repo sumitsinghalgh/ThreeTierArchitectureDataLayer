3a9f253f2854dcf74a1a58f55ed99b4416934f8b
sumitsinghalgh <sumsgl198630pd@gmail.com>
2021-09-24 23:17:50 +0530

    Forth Commitment Done

---
+++ DataAccess/UserDAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObj;
+
+namespace DataAccess
+{
+
+   public class UserDAL
+    {
+        private UserDBEntities objuserDBEntities;
+
+        public UserDAL()
+        {
+            objuserDBEntities = new UserDBEntities();
+        }
+
+        public CustomBO Adduser(UserBO objuserBO)
+        {
+            CustomBO objcustomBO = new CustomBO();
+            User objuser = new User()
+            {
+                UserAddress = objuserBO.UserAddress,
+                UserEmail = objuserBO.UserEmail,
+                UserMobile = objuserBO.UserPhone,
+                UserName = objuserBO.UserName
+            };
+
+            objuserDBEntities.Users.Add(objuser);
+            int returnvalue = objuserDBEntities.SaveChanges();
+
+            if (returnvalue > 0)
+            {
+                objcustomBO.CustomMessage = "Data Successfully Added.";
+                objcustomBO.CustomMessageNumber = returnvalue.ToString();
+            }
+            else 
+            {
+                objcustomBO.CustomMessage = "There is some problem to add user.";
+                objcustomBO.CustomMessageNumber = returnvalue.ToString();
+            }
+
+            return objcustomBO;          
+        
+        }
+
+    }
+}
